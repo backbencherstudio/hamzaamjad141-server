9fcebf5ff4951fb2ee6cbec7bf89e7a249ab0bf8
Md Nasir Uddin Swapan <nasiruddin21211@gmail.com>
2025-06-29 08:52:20 +0600

    user reset otp and weather module updated

--- models/v1/weather/weather.controller.ts
+++ models/v1/weather/weather.controller.ts
@@ -5,9 +5,48 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getWeather = async (req: Request, res: Response) => {
+export const getWeather = async (req, res) => {
+  const { location, status, userId } = req.query;
+  console.log("Received query parameters:", { location, status, userId });
+
+  if (!location || !status || !userId) {
+    res.status(400).json({ message: "Missing required parameters" });
+  }
+
   try {
-    
+    const weatherData = await getWeatherData(location);
+
+    if (status === "HOMEBASE") {
+      const existingWeather = await prisma.weather.findFirst({
+        where: {
+          userId,
+          status,
+        },
+      });
+      console.log("Existing weather data:", existingWeather);
+      if (existingWeather) {
+        res.status(200).json({ message: "Weather data already saved" });
+      } else {
+        const newWeather = await prisma.weather.create({
+          data: {
+            userId,
+            data: weatherData,
+            status,
+          },
+        });
+
+        res.status(201).json(newWeather);
+      }
+    } else {
+      const newWeather = await prisma.weather.create({
+        data: {
+          userId,
+          data: weatherData,
+          status,
+        },
+      });
+      res.status(201).json(newWeather);
+    }
   } catch (error) {
     res.status(500).json({
       success: false,
