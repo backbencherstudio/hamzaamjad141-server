dc0771dcdfe2f4333088a81f5e8ad9561049548b
Tariq-Monowar <hassainseam@gmail.com>
2025-06-30 17:00:35 +0600

    add ai

--- models/v1/ai/ai.routes.ts
+++ models/v1/ai/ai.routes.ts
@@ -2,7 +2,10 @@
 import express from 'express';
  
 import { verifyUser } from "../../../middleware/verifyUsers";
+import { generateAIResponse } from './ai.controllers';
 
 const router = express.Router();
+
+router.get('/generate', generateAIResponse);
  
 export default router;
