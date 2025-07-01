a4e238726c6174889ba7ab3ae05903022d3caca0
Tariq-Monowar <hassainseam@gmail.com>
2025-07-01 11:36:53 +0600

    storage

--- models/v1/portcusts/portcusts.routes.ts
+++ models/v1/portcusts/portcusts.routes.ts
@@ -2,10 +2,12 @@
 import express from 'express';
  
 import { verifyUser } from "../../../middleware/verifyUsers";
+import { createPortcusts } from './portcusts.controllers';
 
 
 const router = express.Router();
 
+router.post("/create", verifyUser('USER'), createPortcusts);
 
  
 export default router;
