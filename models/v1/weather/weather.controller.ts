8014dba099d5291c63d1cfaccba88f0e400293d7
Tariq-Monowar <hassainseam@gmail.com>
2025-07-04 21:49:59 +0600

    fixt

--- models/v1/weather/weather.controller.ts
+++ models/v1/weather/weather.controller.ts
@@ -216,6 +216,7 @@ export const getFavouriteWeather = async (req: any, res: Response) => {
 
 export const deleteFavouriteWeather = async (req: any, res: Response) => {
   const userId = req.user?.userId;
+  const { id } = req.body;
   try {
     if (!userId) {
       res.status(400).json({ message: "Authorization header are required!" });
