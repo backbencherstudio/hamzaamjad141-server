73562da15086a9c71ce02cace54c85af805a945f
Tariq-Monowar <hassainseam@gmail.com>
2025-07-06 10:29:35 +0600

    update

---
+++ __test__/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>
+          
+          //make a input form for login
+
+
+
+        </div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
