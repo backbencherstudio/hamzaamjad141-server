d30d5b0dfc8e9145ff92c4634312e11b3286d433
Tariq-Monowar <hassainseam@gmail.com>
2025-07-14 15:51:17 +0600

    user log summary added

--- models/v1/pilotLogs/pilotlog.routes.ts
+++ models/v1/pilotLogs/pilotlog.routes.ts
@@ -12,11 +12,11 @@ router.get("/get-logbook", verifyUser('ANY'), getLogbook);
 router.post("/addlog-approve/:id", instructorApprov);
 router.post("/addlog-reject/:id", instructorReject);
 
-router.get("/get-logsummary", verifyUser('ANY'), premiumGuard, getLogSummary);
+router.get("/get-logsummary", verifyUser('USER'),  premiumGuard, getLogSummary);
 
 router.delete("/delete-log/:id", verifyUser('ANY'), premiumGuard, deleteLog);
 
-router.get("/get-user-log-summary", verifyUser('USER'), premiumGuard,  getAllUserLogSummaries);
+router.get("/get-user-log-summary", verifyUser('ADMIN'),  getAllUserLogSummaries);
 
 // Add this new route
 router.get("/get-user-logs/:userId", getUserLogs);
