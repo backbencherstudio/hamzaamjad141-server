084e552d318c8debb506aa72d1961f8984961304
Tariq-Monowar <hassainseam@gmail.com>
2025-08-15 18:09:48 +0600

    update

--- utils/subscription.utils.ts
+++ utils/subscription.utils.ts
@@ -1,6 +1,6 @@
 // Subscription utility constants
 export const SUBSCRIPTION_DURATION_MS = 1 * 24 * 60 * 60 * 1000; // 1 day in milliseconds
-export const TRIAL_PERIOD_MS = 2 * 60 * 1000; // 5 minutes trial period in milliseconds
+export const TRIAL_PERIOD_MS = 1 * 60 * 60 * 1000;  // 5 minutes trial period in milliseconds
 
 // Helper function to calculate subscription end date
 export const calculateSubscriptionEndDate = (startDate?: Date): Date => {
